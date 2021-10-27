b8e4c748ac0627ff2233e13a957b41eb71bdaabd
ekokurniadi <ekokurniadi.02@gmail.com>
2021-10-27 13:53:06 +0700

    membuat generate service dan up contoh service hasil generate

---
+++ generate_service.go
@@ -0,0 +1,222 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"reflect"
+	"strings"
+	"time"
+
+	"github.com/ekokurniadi/indodate"
+	"gorm.io/gorm"
+)
+
+func CreateService(db *gorm.DB, mystruct interface{}) string {
+	createFolderService()
+	createFileService(mystruct)
+	writeFileService(mystruct)
+	if nameOfStruct := reflect.TypeOf(mystruct); nameOfStruct.Kind() == reflect.Ptr {
+		return "*" + nameOfStruct.Elem().Name()
+	} else {
+		return nameOfStruct.Name()
+	}
+}
+
+func createFolderService() error {
+	path := filepath.Join("./", "service")
+	_, err := os.Stat(path)
+
+	if os.IsExist(err) {
+		fmt.Println("your directory is already exist but it's ok")
+		return err
+	}
+
+	err = os.Mkdir(path, 0755)
+	if err != nil {
+		fmt.Println("your directory is already exist but it's ok")
+		return err
+	}
+
+	return nil
+}
+
+func createFileService(mystruct interface{}) (string, error) {
+	name := ""
+	if nameOfStruct := reflect.TypeOf(mystruct); nameOfStruct.Kind() == reflect.Ptr {
+		name = nameOfStruct.Elem().Name()
+	} else {
+		name = nameOfStruct.Name()
+	}
+
+	fmt.Println(name)
+
+	filepath, err := filepath.Abs("./service" + "/" + strings.ToLower(name) + ".go")
+	if err != nil {
+		log.Fatal("error")
+		return filepath, err
+	}
+
+	filename, err := os.Create(filepath)
+
+	if err != nil {
+		log.Fatal("Cannot create a file please check your directory again")
+		return filename.Name(), err
+	}
+
+	fmt.Printf("Create %s is successfully \n", filename.Name())
+	return filepath, nil
+}
+
+func writeFileService(mystruct interface{}) (string, error) {
+
+	name := ""
+	if nameOfStruct := reflect.TypeOf(mystruct); nameOfStruct.Kind() == reflect.Ptr {
+		name = nameOfStruct.Elem().Name()
+	} else {
+		name = nameOfStruct.Name()
+	}
+
+	filepath, err := filepath.Abs("./service" + "/" + strings.ToLower(name) + ".go")
+	if err != nil {
+		log.Fatal("error")
+		return filepath, err
+	}
+
+	file, err := os.OpenFile(filepath, os.O_RDWR, 0644)
+	if isError(err) {
+		return "", err
+	}
+	defer file.Close()
+
+	//Write some text line-by-line to file.
+	_, err = file.WriteString("package service")
+	if isError(err) {
+		return "", err
+	}
+	_, err = file.WriteString(fmt.Sprintf("%s%s", "\n", "\n"))
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString(fmt.Sprintf("type %s %s %s", name+"Service", "interface {", "\n"))
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString(fmt.Sprintf("%sServiceCreate(%s %s) (%s %s)%s", name, "input", "input."+name+"Input", "entity."+name, ",error", "\n"))
+	if isError(err) {
+		return "", err
+	}
+	_, err = file.WriteString(fmt.Sprintf("%sServiceUpdate(%s %s,%s %s) (%s %s)%s", name, "inputID", "input.InputID"+name, "inputData", "input."+name+"Input", "entity."+name, ",error", "\n"))
+	if isError(err) {
+		return "", err
+	}
+	_, err = file.WriteString(fmt.Sprintf("%sServiceDeleteByID(%s %s) (%s %s)%s", name, "inputID", "input.InputID"+name, "bool", ",error", "\n"))
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("}\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString(fmt.Sprintf("type %s %s %s", strings.ToLower(name)+"Service", "struct {", "\n"))
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString(fmt.Sprintf("repository %s", "repository."+name+"Repository\n"))
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("}\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString(fmt.Sprintf("func %s %s", "New"+name+"Service(repository repository."+name+"Repository)", "*"+strings.ToLower(name)+"Service {\n"))
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("return " + "&" + strings.ToLower(name) + "Service{repository}\n")
+	if isError(err) {
+		return "", err
+	}
+	_, err = file.WriteString("}\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString(fmt.Sprintf("func (s *%s) %s (%s", strings.ToLower(name)+"Service", name+"ServiceCreate(input input."+name+"Input)", "entity."+name+",error) {\n"))
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString(strings.ToLower(name) + ":=" + "entity." + name + "{}\n")
+	if isError(err) {
+		return "", err
+	}
+	val := reflect.ValueOf(mystruct).Elem()
+	for i := 0; i < val.NumField(); i++ {
+		if val.Type().Field(i).Name == "ID" {
+			_, err = file.WriteString("")
+			if isError(err) {
+				return "", err
+			}
+		} else {
+			fmt.Print("Writting ... ")
+			fmt.Printf("%s%s%s%s%s", strings.ToLower(name), ".", val.Type().Field(i).Name, "=input."+val.Type().Field(i).Name, "\n")
+			_, err = file.WriteString(fmt.Sprintf("%s%s%s%s%s", strings.ToLower(name), ".", val.Type().Field(i).Name, "=input."+val.Type().Field(i).Name, "\n"))
+			if isError(err) {
+				return "", err
+			}
+		}
+	}
+
+	_, err = file.WriteString("new" + name + ",err :=s.repository.Save" + name + "(" + strings.ToLower(name) + ")\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("if err != nil {\n")
+	if isError(err) {
+		return "", err
+	}
+	_, err = file.WriteString("return " + "new" + name + ",err" + "\n")
+	if isError(err) {
+		return "", err
+	}
+	_, err = file.WriteString("}\n")
+	if isError(err) {
+		return "", err
+	}
+	_, err = file.WriteString("return " + "new" + name + ",nil" + "\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("}\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString(fmt.Sprintf("//Generated by Micagen at %v", indodate.LetterDate(time.Now())))
+	if isError(err) {
+		return "", err
+	}
+
+	// Save file changes.
+	err = file.Sync()
+	if isError(err) {
+		return "", err
+	}
+
+	fmt.Printf("Rewrite file is successfully \n")
+	return filepath, nil
+
+}
--- micagen_tes.go
+++ micagen_tes.go
@@ -4,7 +4,8 @@ import (
 	"log"
 	"os"
 
-	"github.com/ekokurniadi/micagen/entity"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -21,14 +22,28 @@ func main() {
 	pass_host := os.Getenv("PASS_HOST")
 	database := os.Getenv("DATABASE")
 	dsn := "" + user_host + ":" + pass_host + "@tcp(" + hostname + ")/" + database + "?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal("Cannot connect the database")
 		return
 	}
 
-	GenerateTable(db, &entity.User{})
-	CreateRepository(db, &entity.User{})
+	// userRepository := repository.NewUserRepository(db)
+	// userService := service.NewUserService(userRepository)
+
+	// fmt.Println(userService)
+	// GenerateTable(db, &entity.User{})
+	// // CreateRepository(db, &entity.User{})
+	// CreateStructInput(&entity.User{})
+	// CreateService(db, &entity.User{})
+
+	router := gin.Default()
+	router.Use(cors.Default())
+	api := router.Group("/api/v1")
+
+	api.POST("/users")
+
+	router.Run()
 
 }
