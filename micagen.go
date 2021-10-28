e9fffed117cb3397f44ae49f6d54d6211a484b6c
ekokurniadi <ekokurniadi.02@gmail.com>
2021-10-28 14:02:47 +0700

    fix error import db

--- micagen.go
+++ micagen.go
@@ -1,7 +1,6 @@
 package micagen
 
 import (
-	"github.com/go-playground/validator/v10"
 	"gorm.io/gorm"
 )
 
@@ -15,41 +14,4 @@ func (micagen *Micagen) GenerateAll(db *gorm.DB, model interface{}) {
 	CreateService(db, model)
 	CreateFormatter(db, model)
 	CreateHandler(db, model)
-}
-
-type Response struct {
-	Meta Meta        `json:"meta"`
-	Data interface{} `json:"data"`
-}
-
-type Meta struct {
-	Message string `json:"message"`
-	Code    int    `json:"code"`
-	Status  string `json:"status"`
-}
-
-func (micagen *Micagen) ApiResponse(message string, code int, status string, data interface{}) Response {
-	meta := Meta{
-		Message: message,
-		Code:    code,
-		Status:  status,
-	}
-
-	jsonResponse := Response{
-		Meta: meta,
-		Data: data,
-	}
-
-	return jsonResponse
-}
-
-func (micagen *Micagen) FormatValidationError(err error) []string {
-	var errors []string
-
-	for _, e := range err.(validator.ValidationErrors) {
-		errors = append(errors, e.Error())
-	}
-
-	return errors
-
 }
