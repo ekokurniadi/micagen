d53bf88102f595507af89e32e31987cb39e92609
ekokurniadi <ekokurniadi.02@gmail.com>
2021-11-13 16:18:28 +0700

    melengkapi generate handler

--- generate_handler.go
+++ generate_handler.go
@@ -208,6 +208,360 @@ func writeFileHandler(mystruct interface{}) (string, error) {
 	if isError(err) {
 		return "", err
 	}
+	_, err = file.WriteString("\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("func (h *" + strings.ToLower(name) + "Handler) Get" + name + "s" + "(c *gin.Context) {\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString(strings.ToLower(name) + "s,err :=h.service." + name + "ServiceGetAll()\n")
+	if isError(err) {
+		return "", err
+	}
+	_, err = file.WriteString("if err !=nil {\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("response :=helper.ApiResponse(\"Failed to get " + name + "s\",http.StatusBadRequest,\"error\",nil)\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("c.JSON(http.StatusBadRequest,response)\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("return\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("}\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("response :=helper.ApiResponse(\"Detail " + name + "s\",http.StatusOK,\"success\",formatter.Format" + name + "s(" + strings.ToLower(name) + "s" + "))\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("c.JSON(http.StatusOK,response)\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("}\n")
+	if isError(err) {
+		return "", err
+	}
+	_, err = file.WriteString("\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("func (h *" + strings.ToLower(name) + "Handler) Create" + name + "(c *gin.Context) {\n")
+	if isError(err) {
+		return "", err
+	}
+	_, err = file.WriteString("var input input." + name + "Input\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("err :=c.ShouldBindJSON(&input)\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("if err !=nil{\n")
+	if isError(err) {
+		return "", err
+	}
+	_, err = file.WriteString("errors:=helper.FormatValidationError(err)\n")
+	if isError(err) {
+		return "", err
+	}
+	_, err = file.WriteString("errorMessage :=gin.H{\"errors\":errors}\n")
+	if isError(err) {
+		return "", err
+	}
+	_, err = file.WriteString("response :=helper.ApiResponse(\"Create " + name + "failed\",http.StatusBadRequest,\"error\",errorMessage)\n")
+	if isError(err) {
+		return "", err
+	}
+	_, err = file.WriteString("c.JSON(http.StatusBadRequest,response)\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("return\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("}\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("new" + name + ",err :=h.service." + name + "ServiceCreate(input)\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("if err !=nil{\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("response :=helper.ApiResponse(\"Create " + name + "failed\",http.StatusBadRequest,\"error\",nil)\n")
+	if isError(err) {
+		return "", err
+	}
+	_, err = file.WriteString("c.JSON(http.StatusBadRequest,response)\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("return\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("}\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("response :=helper.ApiResponse(\"Create " + name + "\",http.StatusOK,\"success\",formatter.Format" + name + "(new" + name + "))\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("c.JSON(http.StatusOK,response)\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("}\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("func (h *" + strings.ToLower(name) + "Handler) Update" + name + "(c *gin.Context) {\n")
+	if isError(err) {
+		return "", err
+	}
+	_, err = file.WriteString("var inputID input.InputID" + name + "\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("err := c.ShouldBindUri(&inputID)\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("if err !=nil {\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("response :=helper.ApiResponse(\"Failed to get " + name + "s\",http.StatusBadRequest,\"error\",nil)\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("c.JSON(http.StatusBadRequest,response)\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("return\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("}\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("var inputData input." + name + "Input\n")
+	if isError(err) {
+		return "", err
+	}
+	_, err = file.WriteString("err = c.ShouldBindJSON(&inputData)\n")
+	if isError(err) {
+		return "", err
+	}
+	_, err = file.WriteString("if err !=nil{\n")
+	if isError(err) {
+		return "", err
+	}
+	_, err = file.WriteString("errors:=helper.FormatValidationError(err)\n")
+	if isError(err) {
+		return "", err
+	}
+	_, err = file.WriteString("errorMessage :=gin.H{\"errors\":errors}\n")
+	if isError(err) {
+		return "", err
+	}
+	_, err = file.WriteString("response :=helper.ApiResponse(\"Update " + name + "failed\",http.StatusBadRequest,\"error\",errorMessage)\n")
+	if isError(err) {
+		return "", err
+	}
+	_, err = file.WriteString("c.JSON(http.StatusBadRequest,response)\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("return\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("}\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("updated" + name + ",err := h.service." + name + "ServiceUpdate(inputID,inputData)\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("if err !=nil {\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("response :=helper.ApiResponse(\"Failed to get " + name + "s\",http.StatusBadRequest,\"error\",nil)\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("c.JSON(http.StatusBadRequest,response)\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("return\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("}\n")
+	if isError(err) {
+		return "", err
+	}
+	_, err = file.WriteString("response :=helper.ApiResponse(\"Update " + name + "\",http.StatusOK,\"success\",formatter.Format" + name + "(updated" + name + "))\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("c.JSON(http.StatusOK,response)\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("}\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("func (h *" + strings.ToLower(name) + "Handler) Delete" + name + "(c *gin.Context) {\n")
+	if isError(err) {
+		return "", err
+	}
+	_, err = file.WriteString("param := c.Param(\"id\")\n")
+	if isError(err) {
+		return "", err
+	}
+	_, err = file.WriteString("id,_ := strconv.Atoi(param)\n")
+	if isError(err) {
+		return "", err
+	}
+	_, err = file.WriteString("_,err := h.service." + name + "ServiceGetByID(id)\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("if err !=nil {\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("response :=helper.ApiResponse(\"Failed to get " + name + "s\",http.StatusBadRequest,\"error\",nil)\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("c.JSON(http.StatusBadRequest,response)\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("return\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("}\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("_,err = h.service." + name + "ServiceDeleteByID(id)\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("if err !=nil {\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("response :=helper.ApiResponse(\"Failed to get " + name + "s\",http.StatusBadRequest,\"error\",nil)\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("c.JSON(http.StatusBadRequest,response)\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("return\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("}\n")
+	if isError(err) {
+		return "", err
+	}
+	_, err = file.WriteString("response :=helper.ApiResponse(\"Delete " + name + "\",http.StatusOK,\"success\",nil)\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("c.JSON(http.StatusOK,response)\n")
+	if isError(err) {
+		return "", err
+	}
+
+	_, err = file.WriteString("}\n")
+	if isError(err) {
+		return "", err
+	}
 
 	// Save file changes.
 	err = file.Sync()
